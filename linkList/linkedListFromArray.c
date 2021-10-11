465cbbe3d63c5f8ba5324749178e7bab695ba70e
Akash sarkar <theakashsarkar7@gmail.com>
2021-10-11 20:28:29 +0600

    link list insert

---
+++ linkList/linkedListFromArray.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Node *CreateLinkedList(int arr[], int size);
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+int main()
+{
+    int a[] = {5,6,30};
+    struct Node *head;
+    head = CreateLinkedList(a,3);
+    while(head != NULL){
+        printf("%d ->", head->data);
+        head = head->next;
+    }
+    printf("NULL\n");
+    return 0;
+}
+
+struct Node *CreateLinkedList(int arr[], int size)
+{
+    struct Node *head = NULL , *temp = NULL, *current = NULL;
+    int i;
+    for(i=0; i< size; i++){
+       temp = (struct Node*)malloc(sizeof(struct Node));
+       temp->data = arr[i];
+       temp->next = NULL;
+       if (head == NULL)
+       {
+           head    = temp;
+           current = temp;
+       }else{
+           current->next = temp;
+           current       = current->next;
+       }
+       
+    }
+    return head;
+}
---
+++ pointer/link.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+    int data;
+    struct  node* link;
+    
+};
+void insert(int x){
+    
+}
+
+int main(){
+       
+}
