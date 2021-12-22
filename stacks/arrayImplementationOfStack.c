90fd03beb8f2b2a4df4b504386bd68ec9422dd2d
Akash sarkar <theakashsarkar7@gmail.com>
2021-12-22 15:23:39 +0600

    pop function added

--- stacks/arrayImplementationOfStack.c
+++ stacks/arrayImplementationOfStack.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 #define MAX 4
 int stack_arr[MAX];
 int top = -1;
@@ -13,12 +14,42 @@ void push(int data)
     top = top + 1;
     stack_arr[top] = data;
 }
+int pop()
+{
+    int value;
+    if(top == -1)
+    {
+        printf("stack underflow");
+        exit(1);
+    }
+    value = stack_arr[top];
+    top = top - 1;
+    return value; //this value 4
+}
+void print()
+{
+    int i;
+    if(top == -1)
+    {
+        printf("stack underflow");
+        return;
+    }
+    for(i = top; i >= 0; i--)
+    {
+        printf("%d ", stack_arr[i]);
+    }
+}
 int main()
 {
+    int data;
     push(1);
     push(2);
     push(3);
     push(4);
-    push(4);
+    //push(4);
+    data = pop();
+    printf("%d \n", data);
+    // data = pop();
+    print();
     return 0;
 }
