924b13bafb658f76bf36ae8c6f6af3d55971ab8e
Akash sarkar <theakashsarkar7@gmail.com>
2022-01-07 00:54:57 +0600

    binary to decimal convert

---
+++ c_programming/binaryTodicmial.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<math.h>
+
+long long convert(int);
+int main()
+{
+    int n = 13;
+    int bin;
+    bin = convert(n);
+    printf("%d in decimal = %lld in binary", n, bin);
+    return 0;
+}
+
+long long convert(int n) {
+  long long bin = 0;
+  int rem, i = 1;
+
+  while (n!=0) {
+    rem = n % 2;
+    n /= 2;
+    bin += rem * i;
+    i *= 10;
+  }
+
+  return bin;
+}
