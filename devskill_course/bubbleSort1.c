79614c41ec124821d300bc7b8d9de527da817213
Akash sarkar <theakashsarkar7@gmail.com>
2022-01-27 01:13:04 +0600

    bubble sort

--- devskill_course/bubbleSort1.c
+++ devskill_course/bubbleSort1.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
+#include<stdio.h>
 void swap(int *a, int *b)
 {
     int tmp = *a;
-    *a = *b;
-    *b = tmp;
+        *a  = *b;
+        *b  = tmp;
 }
 void bubbleSort(int arr[], int n)
 {
@@ -11,9 +11,9 @@ void bubbleSort(int arr[], int n)
     {
         for(int j = i + 1; j < n; j++)
         {
-            if(arr[i] > arr[j])
+            if(arr[i] < arr[j])
             {
-                swap(&arr[i],&arr[j]);
+                swap(&arr[i], &arr[j]);
             }
         }
     }
@@ -23,13 +23,12 @@ void printArray(int arr[], int size)
     for(int i = 0; i < size; i++)
     {
         printf("%d ", arr[i]);
-        printf("\n");
     }
 }
 int main()
 {
-    int arr[] = {64, 34, 50, 25, 12, 22, 11, 90};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int arr[] = {60, 90, 40, 65, 5,85,67, 9};
+    int n   = sizeof(arr) / sizeof(arr[0]);
     bubbleSort(arr, n);
     printf("Sorted array: \n");
     printArray(arr, n);
